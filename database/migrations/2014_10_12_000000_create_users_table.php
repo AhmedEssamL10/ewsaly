0007448fb0aa9d2d74785b08ac8942631d6b6347
AhmedEssamL10 <ahmedessamata77@gmail.com>
2023-12-25 12:13:07 +0200

    create database

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,6 +14,10 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('car_model');
+            $table->string('car_color');
+            $table->string('phone')->unique();
+            $table->string('plate_number')->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
