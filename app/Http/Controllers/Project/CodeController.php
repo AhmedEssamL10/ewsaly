af7310bafb51ad17c37c8fa630f2c70a7a8f955c
AhmedEssamL10 <ahmedessamata77@gmail.com>
2024-01-01 16:45:13 +0200

    updates

--- app/Http/Controllers/Project/CodeController.php
+++ app/Http/Controllers/Project/CodeController.php
@@ -9,20 +9,20 @@ use App\Models\Code;
 
 class CodeController extends Controller
 {
-    public function create(){
-        return view('pages.code.create');
-    }
-    public function store(Request $request){
+    // public function create(){
+    //     return view('pages.code.create');
+    // }
+    // public function store(Request $request){
 
-        $uniqueCode = Str::random(32);
-        // $request->validate([
-        //     'code'=>"required"
-        // ]);
-        Code::create([
-            'code'=>$uniqueCode
-        ]);
-        return view('pages.code.write-code');
-    }
+    //     $uniqueCode = Str::random(32);
+    //     // $request->validate([
+    //     //     'code'=>"required"
+    //     // ]);
+    //     Code::create([
+    //         'code'=>$uniqueCode
+    //     ]);
+    //     return view('pages.code.write-code');
+    // }
     public function check($code){
         $result=Code::where('code',$code)->first();
         if($result->count() > 0){
--- resources/views/pages/code/create.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.parent')
-@section('content')
-<form method="GET" action="{{ route('code.store') }}">
-  @csrf
-    <button type="submit" class="btn btn-primary">Submit</button>
-  </form>
-@endsection
