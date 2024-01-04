14abe4a4bd5c6102fa03ae3564dfc9a9cf7a530c
AhmedEssamL10 <ahmedessamata77@gmail.com>
2024-01-04 14:48:45 +0200

    home page update

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -3,90 +3,197 @@
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>@yield('title')</title>
-
+    <title>home</title>
+    <link
+      href="https://fonts.googleapis.com/css2?family=Cabin:wght@400;500;600;700&family=Rubik+Doodle+Shadow&display=swap"
+      rel="stylesheet"
+    />
     <link rel="stylesheet" href="{{ asset('/dist/output.css') }}" />
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
+      integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
+      crossorigin="anonymous"
+      referrerpolicy="no-referrer"
+    />
   </head>
-  <body>
-    <header class="bg-light shadow-nav shadow-primary_colour mb-4">
-      <nav class="container flex gap-4 justify-between items-center p-4 flex-wrap">
-        <div>
-          <h1 class="text-primary_colour">EWSLY</h1>    
-        </div>
+  <body class="bg-slate-100">
+       <!-- start header -->
+       <header class="mb-4 bg-white">
+        <nav
+          class="container flex gap-4 justify-between items-center p-4 flex-wrap relative"
+        >
+          <div>
+            <h1 class="text-primary_colour">EWSLY</h1>
+          </div>
   
-        <div>
-          <ul class="flex md:gap-4 gap-2 flex-wrap">
+          <div>
+            <button class="flex md:hidden">
+              <i
+                id="open_nav"
+                class="fa-solid fa-bars text-text_primary hover:text-primary_colour fa-xl"
+              ></i>
+            </button>
+          </div>
+  
+          <ul
+            class="md:flex items-center md:gap-4 gap-2 off"
+            id="container_links"
+          >
+            <li class="md:hidden">
+              <button class="absolute top-2 left-2">
+                <i
+                  id="close_nav"
+                  class="fa-solid fa-xmark text-text_primary hover:text-primary_colour fa-xl"
+                ></i>
+              </button>
+            </li>
             <li>
               <a href="{{ route('home') }}"  class="nav-links {{ Route::currentRouteName() === 'home' ? ' active' : '' }} "
-                >Home</a
-              >
+              >Home</a
+            >
+            </li>
+  
+            <li>
+              <a href="" class="nav-links">About</a>
+            </li>
+            <li>
+              <a href="" class="nav-links">Contact us</a>
             </li>
             @auth
-            <li>
-              <a href="{{route('profile.edit')}}" class="nav-links {{ Route::currentRouteName() === 'profile.edit' ? ' active' : '' }} "
-                >Profile</a
-              >
-            </li>
-            @endauth
-         
-            <li>
-              <a href="" class="hover:border-b-2 hover:border-primary_colour"
-                >About</a
-              >
-            </li>
-            <li>
-              <a href="" class="hover:border-b-2 hover:border-primary_colour"
-                >Contact us</a
-              >
-            </li>
-            @auth
-            <li>
-              <form method="POST" action="{{ route('logout') }}">
-                @csrf
 
-                <x-dropdown-link :href="route('logout')"
-                        onclick="event.preventDefault();
-                                    this.closest('form').submit();">
-                    {{ __('Log out') }}
-                </x-dropdown-link>
-            </form>
+            <li class="user_con_links p-4 md:p-0">
+              <b class="">{{ ucfirst(Auth::user()->name) }}</b>
+              <span><i class="fa-solid fa-caret-down"></i></span>
+              <ul class="user_links">
+                <li>
+                  <a href="{{route('profile.edit')}}" class="nav-links {{ Route::currentRouteName() === 'profile.edit' ? ' active' : '' }} "
+                    >Profile</a
+                  >
+                </li>
+                <li>
+                  <a href="" class="nav-links">My massage</a>
+                </li>
+                <li>
+                  <form method="POST" class="nav-links" action="{{ route('logout') }}">
+                    @csrf
+    
+                    <a href="route('logout')"
+                            onclick="event.preventDefault();
+                                        this.closest('form').submit();">
+                        {{ __('Log out') }}
+                  </a>
+                </form>
+                </li>
+              
+              </ul>
             </li>
             @endauth
-            @guest
-            <li>
-              <a href="{{route('login')}}" class="hover:border-b-2 hover:border-primary_colour"
-                >Sign in</a
-              >
-            </li>
-            @endguest
+                @guest
+                <li>
+                  <a href="{{route('login')}}" class="nav-links"
+                    >Sign in</a
+                  >
+                </li>
+                @endguest
+             
           </ul>
-        </div>
-      </nav>
-    </header>
-    <main class="h-screen ">
-      <section>
+        </nav>
+      </header>
+      <!-- end header -->
+      <main class="container p-2 flex flex-col gap-16">
+        <section>
         @yield('content')
-        <p></p>
       </section>
     </main>
   
    
 
-    <footer>
-      <p class="text-text_primary text-center p-2">
-        © 2023, Made with ❤️ by
-        <span
-          ><a
-            href=""
-            class="text-primary_colour border-b-2 border-primary_colour"
-            >KyanLabs</a
-          ></span
-        >
-      </p>
+    <footer class="bg-light mt-4">
+      <div class="container p-6">
+        <div class="grid md:grid-cols-2 grid-cols-1 md:h-[300px]">
+          <div class="h-full flex flex-col p-2 gap-4">
+            <p class="text-text_primary text-[11px] tracking-wider">ADDRESS</p>
+            <p class="text-2xl">
+              290 Maryam Springs 260,
+              <br />
+              Courbevoie, Paris, France
+            </p>
+          </div>
+          <hr class="block md:hidden my-4" />
+          <div class="h-full flex flex-col p-2 gap-5">
+            <p class="text-text_primary text-[11px] tracking-wider">
+              CONTACT US
+            </p>
+            <div>
+              <p class="text-text_primary text-[11px] tracking-wider">
+                CALL US DIRECTLY
+              </p>
+              <p class="text-2xl">(812) 721-1236</p>
+            </div>
+            <div>
+              <p class="text-text_primary text-[11px] tracking-wider">
+                MAIL US DIRECTLY
+              </p>
+              <p class="text-2xl">info@liquid.com</p>
+            </div>
+          </div>
+        </div>
+        <ul class="flex justify-center flex-wrap md:justify-end mt-8 gap-5 ">
+          <li>
+            <a target="_blank" href=""
+              ><i class="fa-brands fa-linkedin text-text_primary"></i>
+              linkedin</a
+            >
+          </li>
+          <li>
+            <a target="_blank" href=""
+              ><i class="fa-brands fa-x-twitter text-text_primary"></i>
+              Linkedin</a
+            >
+          </li>
+          <li><a href="">Privacy Policy</a></li>
+        </ul>
+        <hr class="block my-8" />
+        <p class="text-text_primary text-center p-2">
+          © 2023, Made with ❤️ by
+          <span
+            ><a
+              href=""
+              class="text-primary_colour border-b-2 border-primary_colour"
+              >KyanLabs</a
+            ></span
+          >
+        </p>
+      </div>
     </footer>
-   
+    <!-- end footer -->
+
+    <script>
+      const openNav = document.getElementById("open_nav");
+      const closeNav = document.getElementById("close_nav");
+      const container_links = document.getElementById("container_links");
+      const userConLinks = document.querySelector(".user_con_links");
+      const userLinks = document.querySelector(".user_links");
+      const navLinks = document.querySelector(".nav-links");
+
+      document.addEventListener("click", (e) => {
+        if (
+          e.target === container_links ||
+          e.target === openNav ||
+          e.target === userConLinks ||
+          e.target == userLinks ||
+          e.target == navLinks
+        ) {
+          container_links.classList.add("on");
+          container_links.classList.remove("off");
+        } else {
+          container_links.classList.add("off");
+          container_links.classList.remove("on");
+        }
+      });
+    </script>
   </body>
-  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
   @yield('js')
-  </html>
+
+</html>
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,6 +1,196 @@
 @extends('layouts.header')
 @section('title','Home')
 @section('content')
-
+    <!-- start main -->
+    <main class="container p-2 flex flex-col gap-16">
+        <!-- start section 1 (landing) -->
+        <section class="landing_page">
+          <div class="landing_page_img"></div>
+          <div class="landing_page_overlay"></div>
+          <div
+            class="flex flex-col items-start py-12 h-1/2 max-w-[800px] mx-auto"
+          >
+            <p class="text-white max-w-[450px] text-2xl">
+              <span class="text-primary_colour text-4xl font-cursive">EWSLY</span
+              >, Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              Pariatur, nemo dolorem repellendus.
+            </p>
+            <div class="mt-4">
+              <button class="btn-submit px-6 py-3 rounded-xl">join now</button>
+              <button class="btn-submit px-6 py-3 rounded-xl">Buy QR code</button>
+            </div>
+            <button></button>
+          </div>
+        </section>
+        <!-- end section 1 (landing) -->
+  
+        <!-- start section 2 (Our Services) -->
+        <section
+          class="container flex flex-col gap-6 justify-center items-center"
+        >
+          <div class="flex flex-col gap-2 items-center max-w-[500px]">
+            <p
+              class="text-text_primary bg-white px-2 py-1 rounded-xl text-center"
+            >
+              BUSINESS SOLUTIONS
+            </p>
+            <h1 class="text-primary_colour text-4xl font-bold">Our Services</h1>
+            <p class="text-center">
+              Flexible layouts, and instant results! Choose a pre-built header or
+              create a custom layout that perfectly suits
+              <span class="text-primary_colour text-xl">your needs.</span>
+            </p>
+          </div>
+          <div class="grid grid-cols-2 lg:grid-cols-6 md:grid-cols-4 gap-5">
+            <div class="card_our_services">
+              <div class="box">
+                <i class="fa-solid fa-house icon"></i>
+                <p>Search Engine Opt.</p>
+              </div>
+            </div>
+            <div class="card_our_services">
+              <div class="box">
+                <i class="fa-solid fa-house icon"></i>
+                <p>Search Engine Opt.</p>
+              </div>
+            </div>
+            <div class="card_our_services">
+              <div class="box">
+                <i class="fa-solid fa-house icon"></i>
+                <p>Search Engine Opt.</p>
+              </div>
+            </div>
+            <div class="card_our_services">
+              <div class="box">
+                <i class="fa-solid fa-house icon"></i>
+                <p>Search Engine Opt.</p>
+              </div>
+            </div>
+            <div class="card_our_services">
+              <div class="box">
+                <i class="fa-solid fa-house icon"></i>
+                <p>Search Engine Opt.</p>
+              </div>
+            </div>
+            <div class="card_our_services">
+              <div class="box">
+                <i class="fa-solid fa-house icon"></i>
+                <p>Search Engine Opt.</p>
+              </div>
+            </div>
+          </div>
+        </section>
+        <!-- end section 2 (Our Services) -->
+  
+        <!-- start section 3  (explain)-->
+        <section class="container grid grid-cols-1 gap-8">
+          <article class="explain_con">
+            <div class="card_img md:col-span-2 md:col-start-2"></div>
+            <div class="card md:col-span-3">
+              <p
+                class="text-text_primary bg-slate-100 px-2 py-1 rounded-xl text-center"
+              >
+                SELECTED PROJECTS
+              </p>
+              <h2 class="text-4xl">
+                Magical Design
+                <br />
+                Solutions💡
+              </h2>
+              <p class="max-w-[400px]">
+                Adjust your design through a wide range of theme options in the
+                <span class="text-primary_colour text-xl"
+                  >WordPress Customizer</span
+                >
+                and see the changes instantly.
+              </p>
+              <button class="btn-submit px-6 py-3 rounded-xl">join now</button>
+            </div>
+          </article>
+  
+          <article class="explain_con">
+            <div class="card md:col-span-3 md:col-start-2">
+              <p
+                class="text-text_primary bg-slate-100 px-2 py-1 rounded-xl text-center"
+              >
+                SELECTED PROJECTS
+              </p>
+              <h2 class="text-4xl">
+                Magical Design
+                <br />
+                Solutions💡
+              </h2>
+              <p class="max-w-[400px]">
+                Adjust your design through a wide range of theme options in the
+                <span class="text-primary_colour text-xl"
+                  >WordPress Customizer</span
+                >
+                and see the changes instantly.
+              </p>
+              <button class="btn-submit px-6 py-3 rounded-xl">join now</button>
+            </div>
+            <div class="card_img md:col-span-2"></div>
+          </article>
+        </section>
+        <!-- end section 3  (explain)-->
+  
+        <!-- start section 4  (email adrise)-->
+        <section class="container bg-slate-50 rounded-xl">
+          <div class="flex flex-col justify-center items-center p-10 gap-3">
+            <p
+              class="text-text_primary bg-slate-100 px-2 py-1 rounded-xl text-center"
+            >
+              GET FOR FREE
+            </p>
+            <h3 class="text-center text-text_primary">
+              Have a project in mind?<br />
+              <span class="text-primary_colour text-xl">Let's talk.</span>
+            </h3>
+            <div
+              class="bg-slate-100 w-3/4 gap-8 p-8 grid md:grid-cols-2 grid-cols-1 rounded-xl"
+            >
+              <div>
+                <input
+                  type="text"
+                  name="name"
+                  placeholder="Name"
+                  class="input-field"
+                />
+                <p class="error_masg"></p>
+              </div>
+              <div>
+                <input
+                  type="number"
+                  name="phone"
+                  placeholder="Phone"
+                  class="input-field"
+                />
+                <p class="error_masg"></p>
+              </div>
+              <div>
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="Email address"
+                  class="input-field"
+                />
+                <p class="error_masg"></p>
+              </div>
+              <div class="md:col-span-2">
+                <textarea
+                  placeholder="Write your message"
+                  cols="30"
+                  rows="10"
+                  class="input-field"
+                ></textarea>
+                <p class="error_masg"></p>
+              </div>
+              <button class="btn-submit px-4 mx-auto">Send</button>
+            </div>
+          </div>
+        </section>
+        <!-- end section 4  (email adrise)-->
+      </main>
+      <!-- end main -->
 @endsection
 
